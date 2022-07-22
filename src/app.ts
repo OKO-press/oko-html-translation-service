5454e5a022dcc4c084e0a2dc12ac609fc9bc3ae2
Konrad Fedorczyk <konrad.fedorczyk@oko.press>
2022-07-22 13:30:06 +0200

    Add hello world app

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+import Koa from "koa";
+import Router from "@koa/router";
+
+const PORT = 6660;
+
+const app = new Koa();
+const router = new Router();
+
+router.get('/', (ctx, next) => {
+  ctx.body = 'OKO HTML → DraftJS translation at your service 🦾';
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+app.listen(6660);
+console.log(`Listening on ${PORT}`);
