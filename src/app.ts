7d4e6e6204367362a88be540d22669e632e7b2ff
Konrad Fedorczyk <konrad.fedorczyk@oko.press>
2022-07-22 13:45:01 +0200

    Update app

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = new Koa();
 const router = new Router();
 
 router.get('/', (ctx, next) => {
-  ctx.body = 'OKO HTML → DraftJS translation at your service 🦾';
+  ctx.body = `OKO HTML → DraftJS translation at your service 🦾`;
 })
 
 app
