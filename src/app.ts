77b8ce2d15720d0969c439bd172385366e386e79
Konrad Fedorczyk <konrad.fedorczyk@oko.press>
2022-07-22 15:38:25 +0200

    Add working PoC

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,27 @@
 import Koa from "koa";
 import Router from "@koa/router";
+import bodyParser from "koa-bodyparser";
 
 const PORT = 6660;
 
 const app = new Koa();
 const router = new Router();
 
-router.get('/', (ctx, next) => {
+router.get("/", (ctx, next) => {
   ctx.body = `OKO HTML → DraftJS translation at your service 🦾`;
-})
+});
 
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
+/**
+ * Simple HTML to DraftJS
+ */
+router.post("/translate/simple", async (ctx, next) => {
+  console.log(ctx.request.body?.text);
+  ctx.body = {
+    hello: ctx.request.body?.text
+  };
+});
+
+app.use(bodyParser()).use(router.routes()).use(router.allowedMethods());
 
 app.listen(6660);
 console.log(`Listening on ${PORT}`);
